335396aec2d42bc340c3d09d8953f78fc52751cd
gredlerd <dave.gredler@gmail.com>
2024-07-08 13:34:45 +0200

    advanced participant list design

--- app/components/EventParticipants.tsx
+++ app/components/EventParticipants.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { ParticipantsAccordion } from "./ParticipantsAccordion";
+import { ParticipantsDetails } from "./ParticipantsDetails";
+import { ScrollText } from "lucide-react";
 
 type EventParticipantsProps = {
   closeModal: () => void;
@@ -12,13 +13,18 @@ export const EventParticipants = ({
 }: EventParticipantsProps) => {
   return (
     <div className="fixed top-0 left-0 h-screen w-full bg-gray-200 flex items-center z-50 flex-col justify-between">
-      <div className="w-full h-52 items-center flex justify-center font-bold text-3xl flex-col bg-vsvGray text-white">
-        <p>Teilnehmerliste</p>
-        <span>{title}</span>
+      <div className="w-full h-52 items-center flex justify-center flex-col bg-vsvGray text-white">
+        <div className="flex flex-row gap-4 items-center">
+          <p className=" font-bold text-3xl">Teilnehmerliste</p>
+          <span>
+            <ScrollText />
+          </span>
+        </div>
+        <span className="text-xl pt-5">{title}</span>
       </div>
       <div className="flex flex-col w-full p-5 h-full justify-between">
         <div>
-          <ParticipantsAccordion
+          <ParticipantsDetails
             ConfirmendParticipant={"David Gredler"}
             TentativeParticipant={"Stone"}
             CancelledParticipant={"Unte"}
--- app/components/ParticipantsDetails.tsx
+++ app/components/ParticipantsDetails.tsx
@@ -1,48 +1,54 @@
 import { CircleHelp, ThumbsDown, ThumbsUp } from "lucide-react";
 import React from "react";
 
-type ParticipantsAccordionProps = {
+type ParticipantsDetailsProps = {
   ConfirmendParticipant: string;
   TentativeParticipant: string;
   CancelledParticipant: string;
 };
 
-export const ParticipantsAccordion = ({
+export const ParticipantsDetails = ({
   ConfirmendParticipant,
   TentativeParticipant,
   CancelledParticipant,
-}: ParticipantsAccordionProps) => {
+}: ParticipantsDetailsProps) => {
   return (
     <section className="flex flex-col">
       <div>
-        <div className="flex-row flex justify-between">
-          <span className="text-green-600 flex flex-col">Zusagen</span>
+        <div className="flex-row flex justify-between pt-5">
+          <span className="text-green-600 flex flex-col font-bold text-xl">
+            Zusagen
+          </span>
           <span className="text-green-600">
             <ThumbsUp />
           </span>
         </div>
         <hr className="border-t-2 border-green-600 my-3 w-full" />
-        <span>{ConfirmendParticipant}</span>
+        <span className="text-lg">{ConfirmendParticipant}</span>
       </div>
       <div>
-        <div className="flex-row flex justify-between">
-          <span className="text-vsvGray flex flex-col">Unsicher</span>
+        <div className="flex-row flex justify-between pt-5">
+          <span className="text-vsvGray flex flex-col font-bold text-xl">
+            Unsicher
+          </span>
           <span className="text-vsvGray">
             <CircleHelp />
           </span>
         </div>
         <hr className="border-t-2 border-vsvGray my-3 w-full" />
-        <span>{TentativeParticipant}</span>
+        <span className="text-lg">{TentativeParticipant}</span>
       </div>
       <div>
-        <div className="flex-row flex justify-between">
-          <span className="text-red-600 flex flex-col">Absagen</span>
+        <div className="flex-row flex justify-between pt-5">
+          <span className="text-red-600 flex flex-col font-bold text-xl">
+            Absagen
+          </span>
           <span className="text-red-600">
             <ThumbsDown />
           </span>
         </div>
         <hr className="border-t-2 border-red-600 my-3 w-full" />
-        <span>{CancelledParticipant}</span>
+        <span className="text-lg">{CancelledParticipant}</span>
       </div>
     </section>
   );
